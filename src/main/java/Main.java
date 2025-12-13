354524022389ce35639ec4527bcffa1b45308152
איתן אגרנוביץ <eitan.agranovich@gmail.com>
2025-12-13 12:03:02 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,23 @@ public class Main {
 	static Scanner reader = new Scanner(System.in);
 	public static void main(String[] args) {
         
-        int[] arr = {10, 20, 30, 40, 50, 60};
+        int[] arr = {10, 20, 30, 40, 50, 60, 60};
         int[] arr1 = {11, 21, 31, 41};
+        int[] arrUnsorted = {5, 6, 2, 7, 7, 1};
         Node<Integer> obj = buildNode(arr);
         Node<Integer> obj1 = buildNode(arr1);
         Node<Integer> objEx1 = ex1(obj, obj1);
+        Node<Integer> objUnsorted = buildNode(arrUnsorted);
+        System.out.println("ex1:");
         printNode(objEx1);
+        System.out.println("ex2:");
+        printNode(ex2(objUnsorted));
+        System.out.println("ex3:");
+        System.out.println(ex3(7, objUnsorted));
+	    System.out.println("ex4:");
+        System.out.println(ex4(objUnsorted));
+	    System.out.println("ex5:");
+	    printNode(ex5(obj));
 	}
 
 	
@@ -34,32 +45,145 @@ public class Main {
     }
 	
 
-    public static Node<Integer> ex1(Node<Integer> n1, Node<Integer> n2) {
+    public static Node<Integer> ex1(Node<Integer> list1, Node<Integer> list2) {
         Node<Integer> h = new Node<Integer>(-1);
         Node<Integer> p = h;
-        int min;
+        Node<Integer> x = list1;
+        Node<Integer> y = list2;
     
-        while (n1 != null && n1 != null) {
-            if (n1.getValue() <= n2.getValue()) {
-                min = n1.getValue());
-				n1 = n1.getNext();
-            } else {				
-                min = n2.getValue());
-				n2 = n2.getNext();
-			}
-			p.setNext(new Node<Integer>(min));
-			p = p.getNext();
+        while (x != null && y != null) {
+            if (x.getValue() <= y.getValue()) {
+                p.setNext(new Node<Integer>(x.getValue()));
+                p = p.getNext();
+                x = x.getNext();
+            } else {
+                p.setNext(new Node<Integer>(y.getValue()));
+                p = p.getNext();
+                y = y.getNext();
+            }
         }
-        while (n1 != null) {
-            p.setNext(new Node<Integer>(n1.getValue()));
+    
+        while (x != null) {
+            p.setNext(new Node<Integer>(x.getValue()));
             p = p.getNext();
-            n1 = n1.getNext();
+            x = x.getNext();
         }
-        while (n2 != null) {
-            p.setNext(new Node<Integer>(n2.getValue()));
+    
+        while (y != null) {
+            p.setNext(new Node<Integer>(y.getValue()));
             p = p.getNext();
-            n2 = n2.getNext();
-        }    
+            y = y.getNext();
+        }
+    
         return h.getNext();
     }
-}
+    
+    public static Node<Integer> ex2(Node<Integer> list) {
+        Node<Integer> hNew = new Node<>(null); // רשימה חדשה (dummy head)
+        Node<Integer> t = hNew;
+    
+        while (list != null) {
+            // מציאת מינימום
+            Node<Integer> min = list;
+            Node<Integer> minPrev = null;
+            Node<Integer> p = list;
+    
+            while (p.getNext() != null) {
+                if (p.getNext().getValue() < min.getValue()) {
+                    min = p.getNext();
+                    minPrev = p;
+                }
+                p = p.getNext();
+            }
+    
+            
+            if (minPrev == null) { 
+                list = list.getNext();
+            } else {
+                minPrev.setNext(min.getNext());
+            }
+    
+            
+            min.setNext(null);
+            t.setNext(min);
+            t = min;
+        }
+    
+        return hNew.getNext();
+    }
+    
+    public static int ex3(int x, Node<Integer> l){
+        Node<Integer> p  = l;
+        Node<Integer> h = l;
+        int countBegin = 0;
+        int countEnd = 0;
+        boolean exist = false;
+        while(!exist){
+            if (p.getValue() == x){
+                exist = true;
+            }
+            countBegin++;
+            
+            p = p.getNext();
+        }
+        exist = true;
+        while(exist){
+            if (p.getValue() == x){
+                countEnd = 0;
+            }
+            countEnd++;
+            if (p.getNext() == null){
+                exist = false;
+            } 
+            p = p.getNext();
+        }
+        return countEnd + countBegin;
+        
+    }
+    public static boolean ex4(Node<Integer> list) {
+        Node<Integer> p1 = list;
+    
+        while (p1 != null) {
+            Node<Integer> p2 = p1.getNext();
+            while (p2 != null) {
+                if (p1.getValue().equals(p2.getValue())) {
+                    return false;
+                }
+                p2 = p2.getNext();
+            }
+            p1 = p1.getNext();
+        }
+        return true;
+    }
+    
+    public static Node<Integer> ex5(Node<Integer> list) {
+        Node<Integer> hNew = new Node<>(null); // dummy head
+        Node<Integer> t = hNew;
+    
+        Node<Integer> p = list;
+        while (p != null) {
+            
+            Node<Integer> q = hNew.getNext();
+            boolean found = false;
+    
+            while (q != null && !found) {
+                if (q.getValue().equals(p.getValue())) {
+                    found = true;
+                }
+                q = q.getNext();
+            }
+    
+            
+            if (!found) {
+                t.setNext(new Node<>(p.getValue()));
+                t = t.getNext();
+            }
+    
+            p = p.getNext();
+        }
+    
+        return hNew.getNext();
+    }
+    
+    
+}	
